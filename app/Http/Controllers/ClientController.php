621b3e62ac940144729121bfbf71b36df18a9568
Marcos <marcos.mrpb@gmail.com>
2023-01-18 19:03:03 -0300

    rearranjei a função store

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bill;
 use App\Models\Client;
 use Illuminate\Http\Request;
 
@@ -9,13 +10,23 @@ class ClientController extends Controller
 {
     public function store(Request $request)
     {
+       /*
         $client = Client::create([
             'name' => $request->name,
             'email' => $request->email,
             'phone' => $request->phone,
             'id_number' => $request->id_number
         ]);
-
+        
         return response()->json($client);
+        */
+        Client::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'id_number' => $request->id_number
+        ]);
+
+       
     }
 }
